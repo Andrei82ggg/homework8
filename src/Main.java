83580d5ade30df043a7a68c7fe868fe76ffc29c0
Professional <ustu1982@mail.ru>
2023-05-15 21:13:37 +0500

    two

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,46 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+        System.out.println();
+
+        task2();
+        System.out.println();
+
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+= arr[i];
+        }
+        double average = (double) sum / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила "+average+" рублей");
+    }
+
+    private static void task2() {
+        int[] arr = generateRandomArray();
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>max){
+                max = arr[i];
+            }
+            if (arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила "+min+" рублей. Максимальная сумма трат за день составила "+max+" рублей");
+    }
+
+    private static void task1() {
         int[] arr = generateRandomArray();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
-            System.out.println("Сумма трат за месяц составила "+sum+"рублей");
-        }
+        System.out.println("Сумма трат за месяц составила "+sum+" рублей");
+    }
 
 
     // Объявить метод «сгенерироватьМассив»
